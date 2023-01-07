d1269d61b589cecb0802bde37cbdc44f305fdd5c
ParterOne <valerio.patricolo@gmail.com>
2023-01-07 13:11:57 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 const { defineConfig } = require('vite')
 
 module.exports = defineConfig({
+  base: '/myThreeWebsite/',
   build: {
     rollupOptions: {
       input: {
         main: './index.html',
+        works: './works.html',
+        contacts: './contacts.html',
         //contacts: './contacts.html',
         //works: './works.html',
         // ...
